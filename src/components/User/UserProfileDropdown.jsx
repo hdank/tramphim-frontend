6f72280cb86dbdc78fe76b42aaceffabb0861855
hdank <vohaidang685@gmail.com>
2025-11-30 21:47:40 +0700

    add: doi-qua page

--- src/components/User/UserProfileDropdown.jsx
+++ src/components/User/UserProfileDropdown.jsx
@@ -135,6 +135,17 @@ export default function UserProfileDropdown() {
                             💰 Kiếm Đậu
                         </a>
                         <a
+                            href={user ? "/doi-qua" : "/dang-nhap"}
+                            className="group flex items-center px-4 py-2 text-sm text-gray-300 hover:bg-white/10 hover:text-white"
+                        >
+                            <div className="mr-3 h-5 w-5 flex items-center justify-center text-orange-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                                    <path d="M9.375 3a1.875 1.875 0 000 3.75h1.875v4.5H3.375A1.875 1.875 0 011.5 9.375v-.75c0-1.036.84-1.875 1.875-1.875h3.193A3.375 3.375 0 0112 2.753a3.375 3.375 0 015.432 3.997h3.193c1.035 0 1.875.84 1.875 1.875v.75c0 1.036-.84 1.875-1.875 1.875H12.75v-4.5h1.875a1.875 1.875 0 10-1.875-1.875V6.75h-1.5V4.875C11.25 3.839 10.41 3 9.375 3zM11.25 12.75H3v6.75a2.25 2.25 0 002.25 2.25h6v-9zM12.75 12.75v9h6a2.25 2.25 0 002.25-2.25v-6.75h-8.25z" />
+                                </svg>
+                            </div>
+                            🎁 Đổi Quà
+                        </a>
+                        <a
                             href={user ? "/mua-premium" : "/dang-nhap"}
                             className="group flex items-center px-4 py-2 text-sm text-gray-300 hover:bg-white/10 hover:text-white"
                         >
