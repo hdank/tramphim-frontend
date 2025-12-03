07390498160cfa2e090aea1a01d300427d433588
hdank <vohaidang685@gmail.com>
2025-12-03 22:01:39 +0700

    fix: rename the ad button to auto skip intro

--- src/components/ServerButton/ServerButtons.jsx
+++ src/components/ServerButton/ServerButtons.jsx
@@ -34,21 +34,20 @@ function SkipIntroButton({ initialAutoSkip = true }) {
       onClick={toggleSkipIntro}
       className="S flex flex-row items-center justify-center gap-2 rounded-md px-1.5 py-0.5 text-xs font-normal text-white transition-all duration-300 lg:px-5"
     >
-      Tắt QC
+      Tự động bỏ qua intro
       <span
         className={`text-xs ${
           autoSkipIntro
-            ? "text-sky-300" // ON: viền cam, chữ xanh
-            : "text-white" // OFF: viền xám, chữ trắng
+            ? "text-sky-300" // ON: màu xanh
+            : "text-white" // OFF: màu trắng
         }`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          className="h-7 w-7 fill-current" // <-- quan trọng: lấy màu từ text color
+          viewBox="0 0 24 24"
+          className="h-5 w-5 fill-current"
         >
-          <path d="M11.35 8.337c0-.699-.42-1.138-1.001-1.138c-.584 0-.954.444-.954 1.239v.453c0 .8.374 1.248.972 1.248c.588 0 .984-.44.984-1.2zm-5.413.237l-.734-2.426H5.15l-.734 2.426h1.52z" />
-          <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm6.209 6.32c0-1.28.694-2.044 1.753-2.044c.655 0 1.156.294 1.336.769h.053v-2.36h1.16V11h-1.138v-.747h-.057c-.145.474-.69.804-1.367.804c-1.055 0-1.74-.764-1.74-2.043v-.695zm-4.04 1.138L3.7 11H2.5l2.013-5.999H5.9L7.905 11H6.644l-.47-1.542H4.17z" />
+          <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v6.62c0 1.44 1.555 2.343 2.805 1.628L12 12.938v3.372c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 5.716 12 6.62 12 8.06v3.372L5.055 7.06Z" />
         </svg>
       </span>
     </button>
